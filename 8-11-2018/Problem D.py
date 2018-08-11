a3123331647882740c93c259d80706b1fea261ec
christopherhui <christophergkhui@gmail.com>
2018-08-11 14:03:04 -0700

    soln. beginning

--- 8-11-2018/Problem D.py
+++ 8-11-2018/Problem D.py
@@ -1,12 +1,24 @@
 aList = [int(x) for x in input().split()]
 n = aList[0]
 p = aList[1]
+count = 1
 
 carDistReal = [int(x) for x in input().split()]
 carDistReal.sort()
 
+
 carDistExp = []
 for i in range(n):
-    carDistExp.append(p * n + p)
+    carDistExp.append(p * i + p)
+
+for i in range(n):
+    if carDistReal[i] >= carDistExp[i]:
+        continue
+    else:
+        difference = carDistExp[i] - carDistReal[i]
+        carDistReal = list(map(lambda x: x + difference, carDistReal))
+        count += difference
+
+
 
 
