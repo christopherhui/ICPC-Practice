0ac091720d4030a6fa221bcbe86990748eb329a7
christopherhui <christophergkhui@gmail.com>
2018-08-11 11:15:31 -0700

    fixed soln.

--- 8-11-2018/Problem F.py
+++ 8-11-2018/Problem F.py
@@ -2,10 +2,9 @@ n = int(input())
 aList = []
 for i in range(0, n):
     problem = input()
-    if problem == "P = NP":
+    if problem == "P=NP":
         print("skipped")
     else:
-        for c, val in enumerate(aList, 0):
-            if c == '+':
-                print(int(str[:val]) + int(str[val + 1:]))
-
+        for c, val in enumerate(problem, 0):
+            if val == '+':
+                print(int(problem[:c]) + int(problem[c + 1:]))
