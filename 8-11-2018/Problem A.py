1be548560faaf4f19130aec74718b6dd78b7bacf
christopherhui <christophergkhui@gmail.com>
2018-08-11 13:13:37 -0700

    update on soln.

--- 8-11-2018/Problem A.py
+++ 8-11-2018/Problem A.py
@@ -8,37 +8,35 @@ for i in range(n):
     b = input().split()
     tree.append(b)
 
-for i in range(n):
-    famTree = tree[i]
-    if famTree[1] in map:
-        if famTree[2] in map:
-            map[famTree[0]] = (map[famTree[1]] + map[famTree[2]]) / 2
-        else:
-            map[famTree[0]] = (map[famTree[1]]) / 2
-            map[famTree[2]] = 0
+repeat = False
 
-    elif famTree[2] in map:
+while not repeat:
+    for i in range(n):
+        famTree = tree[i]
         if famTree[1] in map:
-            map[famTree[0]] = (map[famTree[1]] + map[famTree[2]]) / 2
-        else:
-            map[famTree[0]] = (map[famTree[2]]) / 2
-            map[famTree[1]] = 0
+            if famTree[2] in map:
+                temp1 = map[famTree[1]]
+                temp2 = map[famTree[2]]
+                map[famTree[0]] = (map[famTree[1]] + map[famTree[2]]) / 2
+            else:
+                temp1 = map[famTree[1]]
+                map[famTree[0]] = (map[famTree[1]]) / 2
+                map[famTree[2]] = 0
+                temp2 = map[famTree[2]]
 
-for i in range(n):
-    famTree = tree[i]
-    if famTree[1] in map:
-        if famTree[2] in map:
-            map[famTree[0]] = (map[famTree[1]] + map[famTree[2]]) / 2
-        else:
-            map[famTree[0]] = (map[famTree[1]]) / 2
-            map[famTree[2]] = 0
+        elif famTree[2] in map:
+            if famTree[1] in map:
+                temp1 = map[famTree[1]]
+                temp2 = map[famTree[2]]
+                map[famTree[0]] = (map[famTree[1]] + map[famTree[2]]) / 2
+            else:
+                temp2 = map[famTree[2]]
+                map[famTree[0]] = (map[famTree[2]]) / 2
+                map[famTree[1]] = 0
+                temp1 = map[famTree[1]]
 
-    elif famTree[2] in map:
-        if famTree[1] in map:
-            map[famTree[0]] = (map[famTree[1]] + map[famTree[2]]) / 2
-        else:
-            map[famTree[0]] = (map[famTree[2]]) / 2
-            map[famTree[1]] = 0
+        if temp1 != map[famTree[0]] or temp2 != map[famTree[0]]:
+            repeat = True
 
 max = 0
 maxPer = ''
