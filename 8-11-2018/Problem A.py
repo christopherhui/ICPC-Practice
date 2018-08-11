b7065b05122868123de09c668cdfa08f93b902d8
christopherhui <christophergkhui@gmail.com>
2018-08-11 12:59:38 -0700

    update on soln.

--- 8-11-2018/Problem A.py
+++ 8-11-2018/Problem A.py
@@ -3,10 +3,29 @@ n, m = aList[0], aList[1]
 map = {}
 king = input()
 map[king] = 1
-
+tree = []
+for i in range(n):
+    b = input().split()
+    tree.append(b)
 
 for i in range(n):
-    famTree = input().split()
+    famTree = tree[i]
+    if famTree[1] in map:
+        if famTree[2] in map:
+            map[famTree[0]] = (map[famTree[1]] + map[famTree[2]]) / 2
+        else:
+            map[famTree[0]] = (map[famTree[1]]) / 2
+            map[famTree[2]] = 0
+
+    elif famTree[2] in map:
+        if famTree[1] in map:
+            map[famTree[0]] = (map[famTree[1]] + map[famTree[2]]) / 2
+        else:
+            map[famTree[0]] = (map[famTree[2]]) / 2
+            map[famTree[1]] = 0
+
+for i in range(n):
+    famTree = tree[i]
     if famTree[1] in map:
         if famTree[2] in map:
             map[famTree[0]] = (map[famTree[1]] + map[famTree[2]]) / 2
