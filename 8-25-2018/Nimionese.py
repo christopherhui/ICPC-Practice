ec83a758c77c953bf1ddd3a1b43e2f1feda12602
christopherhui <christophergkhui@gmail.com>
2018-08-25 15:24:32 -0700

    this is just getting crazier

--- 8-25-2018/Nimionese.py
+++ 8-25-2018/Nimionese.py
@@ -30,19 +30,19 @@ for c, substring in enumerate(string, 0):
                     newnewString = z + newnewString[1:]
                     hardlet = z.lower()
                 else:
-                    hardlet = newnewString[0]
+                    hardlet = newnewString[0].lower()
             elif d == 0:
                 if newnewString[0] not in softconsonants:
                     z = find_closest(val[0], softconsonants)
                     newnewString = z + newnewString[1:]
                     hardlet = z.lower()
                 else:
-                    hardlet = newnewString[0]
+                    hardlet = newnewString[0].lower()
             elif newnewString[0] in softconsonants and hardlet != -1:
                 newnewString = hardlet + newnewString[1:]
-            if newnewString[len(val) - 1] in hardconsonants:
-                newnewString = newnewString[len(val) - 1] + find_closest(newnewString[len(val) - 1], vowels) + 'h'
-        bulbul[d] = newnewString
+            if newnewString[len(val) - 1] in hardconsonants or newString[len(substring) - 1] in softconsonants and d == len(bulbul) -1:
+                newnewString = newnewString[:len(val)] + find_closest(newnewString[len(val) - 1], vowels) + 'h'
+            bulbul[d] = newnewString
         string[c] = ''.join(bulbul)
 
     else:
@@ -51,8 +51,8 @@ for c, substring in enumerate(string, 0):
                 newString = find_closest(substring[0], hardconsonants) + substring[1:]
         elif newString[0] not in softconsonants:
             newString = find_closest(substring[0], softconsonants) + substring[1:]
-        if newString[len(substring) - 1] in hardconsonants:
-            newString = newString[len(substring) -1] + find_closest(newString[len(substring) - 1], vowels) + 'h'
+        if newString[len(substring) - 1] in hardconsonants or newString[len(substring) - 1] in softconsonants:
+            newString = newString[:len(substring)] + find_closest(newString[len(substring) - 1], vowels) + 'h'
         string[c] = newString
 
 print(' '.join(string))
