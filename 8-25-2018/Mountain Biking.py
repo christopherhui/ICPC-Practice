29f2313d099e0146b03adfe48929e998b32ad3e4
christopherhui <christophergkhui@gmail.com>
2018-08-25 12:28:59 -0700

    lots of physics

---
+++ 8-25-2018/Mountain Biking.py
@@ -0,0 +1,27 @@
+import math
+'''
+a = math.cos(1.0472) * 9
+b = math.sqrt(((2)*(a)*(100)))
+print(b)
+
+c = math.sqrt( 2 * 4.5 * 69 + 30 ** 2 )
+print(c)
+'''
+aList = [int(x) for x in input().split()]
+n = aList[0]
+g = aList[1]
+slopeList = []
+prevvel = 0
+veloList = []
+for _ in range(n):
+    slopeList.append([int(x) for x in input().split()])
+for i in range(n - 1, -1, -1):
+    slope = slopeList[i]
+    rad = math.pi * slope[1] / 180
+    dist = slope[0]
+    accel = g * math.cos(rad)
+    vel = math.sqrt( prevvel ** 2 + 2 * accel * dist )
+    veloList.append(vel)
+    prevvel = vel
+for i in range(n - 1, -1, -1):
+    print(round(veloList[i], 5))
