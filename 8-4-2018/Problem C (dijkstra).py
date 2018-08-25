9f270e8b037eea4e39d74ff342bec22908be16d2
christopherhui <christophergkhui@gmail.com>
2018-08-24 22:38:52 -0700

    attempted solution in determining path: wrong answer on 6

--- 8-4-2018/Problem C (dijkstra).py
+++ 8-4-2018/Problem C (dijkstra).py
@@ -11,29 +11,42 @@ class Graph:
     def min_distance(self, distanceList, visited):
 
         min = math.inf
+        min_index = -1
         for v in range(self.vertices):
             if distanceList[v] < min and visited[v] == False:
                 min = distanceList[v]
                 min_index = v
         return min_index
 
+    def print_solution(self, parent, j):
+        if parent[j] == -1:
+            print(j, end=' ')
+            return
+        self.print_solution(parent, parent[j])
+        print(j, end=' ')
+
     def dijkstra(self, s):
 
         visited = [False] * (self.vertices)
         distanceList = [math.inf] * (self.vertices)
         distanceList[s] = 0
+        parent = [-1] * self.vertices
 
         for i in range(self.vertices - 1):
 
             u = self.min_distance(distanceList, visited)
+            if u == -1:
+                print(-1)
+                return
             visited[u] = True
 
             for v in range(self.vertices):
                 if self.graph[u][v] > 0 and visited[v] == False and \
-                    distanceList[v] > distanceList[u] + self.graph[u][v]:
+                        distanceList[v] > distanceList[u] + self.graph[u][v]:
                     distanceList[v] = distanceList[u] + self.graph[u][v]
+                    parent[v] = u
 
-        return distanceList
+        self.print_solution(parent, self.vertices - 1)
 
 aList = [int(x) for x in input().split()]
 n = aList[0]
@@ -42,14 +55,4 @@ graph = Graph(n)
 for _ in range(m):
     bList = [int(x) for x in input().split()]
     graph.add_edge(bList[0], bList[1], bList[2])
-print(graph.dijkstra(1))
-
-'''
-5 6
-1 2 2
-2 5 5
-2 3 4
-1 4 1
-4 3 3
-3 5 1
-'''
+graph.dijkstra(1)
