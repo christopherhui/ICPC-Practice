616f12c883c9ab85cb3b5c5a1f2e467e2ddf3a02
christopherhui <christophergkhui@gmail.com>
2018-09-01 11:30:16 -0700

    overflow error for arbitrary sized values

--- 9-1-2018/Coin Change.py
+++ 9-1-2018/Coin Change.py
@@ -2,16 +2,15 @@ import math
 
 aList = [int(x) for x in input().split()]
 n, s = aList[0], aList[1]
-if n != 0:
-    coins = [int(x) for x in input().split()]
-    for i in range(1, len(coins)):
-        coins[i] = coins[i - 1] * coins[i]
-    coins = [1] + coins
-else:
-    coins = [1]
+coins = [int(x) for x in input().split()]
+for i in range(1, len(coins)):
+    coins[i] = coins[i - 1] * coins[i]
+coins = [1] + coins
 
-dp = [1] * (coins[0] + 1) + [math.inf] * (s - coins[0])
-dp[0] = 0
+dp = [0] * s
+for a in range(s):
+    dp[a] = a
+dp = dp + [s]
 
 for j in range(s + 1):
     for change in [x for x in coins if j >= x]:
