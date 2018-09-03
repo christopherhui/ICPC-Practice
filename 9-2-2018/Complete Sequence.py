231a8ceed6f24132ae3fc03c220d5295d5c3e649
christopherhui <christophergkhui@gmail.com>
2018-09-02 23:42:28 -0700

    it's fine, after taking me 4 HOURS to finish a single question

--- 9-2-2018/Complete Sequence.py
+++ 9-2-2018/Complete Sequence.py
@@ -2,38 +2,41 @@ aList = [int(x) for x in input().split()]
 k, n = aList[0], aList[1]
 data = [int(x) for x in input().split()]
 
-counts = {}
-for i in range(1, k+1):
-    counts[i] = 0
-for i in data:
-    counts[i] += 1
+def isSolution(counts):
+    start = 0
+    for key in counts:
+        if start == 0:
+            same = counts[key]
+            start += 1
+        elif same != counts[key]:
+            return False
+    return True
 
-countsofcounts = {}
-for num, count in counts.items():
-    if count not in countsofcounts:
-        countsofcounts[count] = 1
-    else:
-        countsofcounts[count] += 1
+def getCounts(data):
+    counts = {}
+    for i in range(1, k+1):
+        counts[i] = 0
+    for j in data:
+        counts[j] += 1
+    return counts
 
-counter = 0
-target = max(countsofcounts, key=countsofcounts.get)
-add, sub = '', ''
-
-for num, count in counts.items():
-    if count != target:
-        if count < target:
-            add = "+" + str(num)
-        else:
-            sub = "-" + str(num)
-        counter += abs(count - target)
-
-if counter >= 3:
-    print("*")
-elif counter == 2:
-    if sub != '' or add != '':
-        print(sub, add)
-elif counter == 1:
-    if add == '':
-        print(sub)
-    else:
-        print(add)
+counts = getCounts(data)
+found = False
+x = min(counts, key=counts.get)
+y = max(counts, key=counts.get)
+if isSolution(getCounts(data + [x])):
+    print('+'+str(x))
+    found = True
+a = data.copy()
+a.pop(a.index(y))
+if isSolution(getCounts(a)) and not found:
+    print('-'+str(y))
+    found = True
+b = data.copy()
+b += [x]
+b.pop(b.index(y))
+if isSolution(getCounts(b)) and not found:
+    print('-'+str(y), '+'+str(x))
+    found = True
+if not found:
+    print('*')
