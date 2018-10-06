6ffb8057a3a4822abd8766db28794b955464e92d
christopherhui <christophergkhui@gmail.com>
2018-10-06 12:27:22 -0700

    why did i hardcode this

--- 10-6-2018/Left and Right.py
+++ 10-6-2018/Left and Right.py
@@ -1,54 +1,104 @@
-n = int(input())
-order = input()
-visited = {}
+def movDown(i, pos):
+  pos -= 1
+  while visited[pos] == True:
+    pos -= 1
+  return pos
 
-for i in range(n):
+def moveUp(i, pos):
+  pos += 1
+  while visited[pos] == True:
+    pos += 1
+  return pos
+
+n = int(input())
+mov = input()
+prevPos = 1
+pos = 1
+visited = {}
+for i in range(n+1):
   visited[i] = False
 
-pos = 1
-for i in range(0, n-1):
-  if order[i] == 'L':
-    pos += 1
-  else:
-    break
-visited[pos-1] = True
-print(pos)
-
-for c, val in enumerate(order, 0):
-  if c+1 > n-1:
-      print(pos)    
-      
-  elif order[c+1] == val:
-    j = 0
-    if val == 'L':
-      j = -1
-      while visited[pos+j] == True:
-        pos -= 1
-        j -= 1
-      print(pos)
-      visited[pos-1] = True     
-    if val == 'R':
-      j = 1
-      while visited[pos+j] == True:
-        pos += 1
-        j += 1
-      print(pos)
-      visited[pos-1] = True
-
-  elif order[c+1] != val:
-    if val == 'L':
-      i = 0
-      pos -= 1
-      while c+1+i < n-1 and order[c+1+i] != val:
-        pos -= 1
-        i += 1
-      print(pos)
-      visited[pos-1] = True       
-    if val == 'R':
-      i = 0
+for i in mov:
+   if i == 'L':
       pos += 1
-      while c+1+i < n-1 and order[c+1+i] != val:
-        pos += 1
-        i += 1
+   else:
+      break
+print(pos)
+visited[pos] = True
+
+for c, val in enumerate(mov, 0):
+  if c+1 < n-1 and mov[c+1] == val:
+    if val == 'L':
+      pos = movDown(c, pos)
       print(pos)
-      visited[pos-1] = True       
+      visited[pos] = True
+
+    if val == 'R':
+      pos = moveUp(c, pos)
+      print(pos)
+      visited[pos] = True
+
+  elif c+2 < n-1 and mov[c+1] != val and mov[c+2] != val:
+
+    if mov[c+1] == 'L':
+      count = 0
+      for j in range(c+1, n-1):
+        if mov[j] != val:
+          count += 1
+        else:
+          break
+      pos += count + 1
+      print(pos)
+      visited[pos] = True
+
+    if mov[c+1] == 'R':
+      count = 0
+      for j in range(c+1, n-1):
+        if mov[j] != val:
+          count -= 1
+        else:
+          break
+      pos += count - 1
+      print(pos)
+      visited[pos] = True
+      
+  elif c+1 < n-1 and mov[c+1] != val and c != 0:
+    if mov[c+1] == 'L':
+      count = 0
+      counter = 0
+      for j in range(c+1, n-1):
+        if mov[j] != val:
+          count += 1
+          counter += 1
+        else:
+          break
+      if counter != 1: pos += count + 1
+      else: pos += count
+      print(pos)
+      visited[pos] = True
+
+    if mov[c+1] == 'R':
+      count = 0
+      counter = 0
+      for j in range(c+1, n-1):
+        if mov[j] != val:
+          count += 1
+          counter += 1
+        else:
+          break
+      if counter != 1: pos += count + 1
+      else: pos += count
+      print(pos)
+      visited[pos] = True
+
+  
+  else:
+    if val == 'L':
+      pos = movDown(c, pos)
+      print(pos)
+      visited[pos] = True
+
+    if val == 'R':
+      pos = moveUp(c, pos)
+      print(pos)
+      visited[pos] = True
