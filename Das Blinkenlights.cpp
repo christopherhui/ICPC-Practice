371c897f9c0c84f4532927b1bc6186706c8eb710
christopherhui <christophergkhui@gmail.com>
2018-10-06 11:07:35 -0700

    Create Das Blinkenlights.cpp

---
+++ Das Blinkenlights.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int gcd(int a, int b)
+{
+    for (;;)
+    {
+        if (a == 0) return b;
+        b %= a;
+        if (b == 0) return a;
+        a %= b;
+    }
+}
+
+int lcm(int a, int b)
+{
+    int temp = gcd(a, b);
+
+    return temp ? (a / temp * b) : 0;
+}
+
+int main() {
+  ios::sync_with_stdio(0);
+    cin.tie(0); 
+  cout.tie(0);
+
+  int a,b,c;
+  cin >> a >> b >> c;
+  if (lcm(a, b) <= c) {
+    cout << "yes";
+  }
+  else
+    cout << "no";
+
+  return 0;
+}
