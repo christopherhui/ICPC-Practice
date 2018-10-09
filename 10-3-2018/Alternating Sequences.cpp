a5ccdde8609979ac60d5380eddb4f006c8e4c555
christopherhui <christophergkhui@gmail.com>
2018-10-09 09:39:41 -0700

    needs to have greatest common subsequence (not implemented)

---
+++ 10-3-2018/Alternating Sequences.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main() {
+  ios::sync_with_stdio(0);
+	cin.tie(0);
+  cout.tie(0);
+
+  int n;
+  cin >> n;
+  vector<int> v;
+  bool status;
+  int max = 0;
+
+  while (n--) {
+    int a;
+    int b = 0;
+    cin >> b;
+    v.push_back(b);
+    a++;
+  }
+  if (v[0] > 0) {
+    status = 1;
+    max++;
+  }
+  else {
+    status = 0;
+    max++;
+  }
+
+  for (int i = 1; i < v.size(); i++) {
+    if (v[i] > 0 && !status) {
+      status = 1;
+      max++;
+    }
+    else if(v[i] < 0 && status) {
+      status = 0;
+      max++;
+    }
+  }
+  cout << max;
+
+  return 0;
+}
