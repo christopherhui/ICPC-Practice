e2f1788bafe12b95433ef27ee896099151c68df8
christopherhui <christophergkhui@gmail.com>
2018-11-10 15:19:32 -0800

    wth

--- 2018 Arabella/Shortest Path.cpp
+++ 2018 Arabella/Shortest Path.cpp
@@ -12,11 +12,11 @@ int main() {
   while (t--) {
     long double a, b, c, x;
     cin >> a >> b >> c >> x;
-    long double one = sqrt(pow(c, 2)+pow(a,2)/2)+sqrt(pow((b+c),2)+pow(a,2)/2);
-    long double two = (x/100)*(sqrt(pow(b,2)+pow(a,2)));
-    long double three = sqrt(pow(((x-100)/100)*(a/2),2)+pow(((x-100)/100)*(b),2)+pow(c,2));
-    long double four = sqrt(pow(((x-100)/100)*(a/2),2)+pow(c,2));
-    cout << fixed << setprecision(10) << one+two+three+four << endl;
+    long double one = sqrt(pow(a,2)+pow((b+2*c),2));
+    long double two = sqrt((pow((a*(x/100)),2))+pow((b*(x/100)),2));
+    long double three = sqrt(pow(((x-100)/100)*a,2)
+    +pow((((x-100)/100)*b+2*c),2));
+    cout << fixed << setprecision(10) << one + two + three;
   }
   return 0;
 }
